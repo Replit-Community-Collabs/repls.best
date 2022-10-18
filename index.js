f2f3deb2ca0687adc3cd3967d77db71265dd496d
Dillon Barnes <83948303+DillonB07@users.noreply.github.com>
2022-10-18 16:25:28 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,205 @@
+// also who's getting the domain, it's not cheap
+// its 3.99 on godaddy https://www.godaddy.com/domainsearch/find?domainToCheck=repls.best&tmskey=1dom_03_buydomain
+
+// cant make subdomains of the Repl's url ah true
+
+// Maybe its an idea to add a option to manually get a repl.best/gold subdomain? Like if you have ... likes but didn't got on trending? Or something like that. (Raadsel)
+
+// Also what about malicious repls. The entire domain could be marked as malicious if one subdomain decides to do some stuff. + if we get a subdomain for root & www we could add a form where people who got a subdomain want it to be shorter (ex: amongus-remake.repls.best) -- Raadsel
+
+// We can cover ourselves from getting blacklisted by saying we don't condone anything on the subdomains, but we still need to protect others by watching for malicious subdomains. -- Will that work?
+
+// CatR3kd here, I added a JSON DB with a backup that automatically checks for empty files. Also, I made sure that no repl can appear twice, it just gets updated. For malicious repls, we can either do it with humans (moderators), or programmatically, which is the dream. Also, should we make a discord server and use that to communicate, and have a bot send the new domains in for verification? And, how are we contacting them? Discord bot? Replit bot? 
+
+//For contacting I think a Replit bot (maybe a new one or G'day bot or something) if we want it to be automatic (which we want). Discord bot to contact people would even more sounds like a scam. And if you want you can make a discord server. Maybe we could moderate malicious repls also via Discord. But it would be unlikely that there will be a malicious repl on trending + there are not that much repls on trending so it would be easy to do with humans.
+
+// Are we gonna use express as website backend? + will the frond-end be in this repl or another one. I dont know how the automatic subdomain things go and if thats possible -- Raadsel
+
+const fs = require('fs');
+const trendingReplsQuery = fs.readFileSync('GQL/trendingRepls.graphql', 'utf-8');
+const getReplURLQuery = fs.readFileSync('GQL/getReplURL.graphql', 'utf-8');
+const sendCommentMutation = fs.readFileSync('GQL/getReplURL.graphql', 'utf-8');
+
+// Better GQL Function
+// Courtesy @RayhanADev <3
+
+const { lightfetch } = require('lightfetch-node');
+
+class GraphQL {
+	constructor(token) {
+		this.headers = {
+			'X-Requested-With': 'XMLHttpRequest',
+			'Referrer': 'https://replit.com/',
+			'Cookie': token ? `connect.sid=${token};` : '',
+		};
+	}
+	async request(query, variables={}) {
+		const { data, errors } = await lightfetch('https://replit.com/graphql', {
+			method: 'POST',
+			headers: {
+        ...this.headers
+      },
+			body: {
+				query,
+				variables: JSON.stringify(variables),
+			}
+		}).then(async (res) => {
+      let data = await res.text();
+
+      //console.log(data); // IF YOU NEED TO DEBUG AN ERROR, UNCOMMENT THIS LINE
+
+      return res.json()
+    }).catch(e => {
+      return { data: {}, errors: [e] }
+    });
+
+		if (errors) throw new Error('GraphQL Error: \n[\n  ' + errors.join(',\n  ') + '\n]');
+		return data;
+	}
+}
+
+const client = new GraphQL();
+
+function updateData(){
+  fs.writeFileSync('Data/Current.json', JSON.stringify({}));
+  client.request(trendingReplsQuery).then(data => {
+    let repls = data.trendingReplPosts.map(({repl}) => repl.id)
+    repls.forEach(repl => {
+      client.request(getReplURLQuery, { id: repl }).then(data => {
+        saveToDB(data.repl);
+        
+        let message;
+        let domain = data.repl.slug.toLowerCase()+'.repls.best'
+        if (data.repl.config.isServer || data.repl.language == "html") {
+          // How do I make this not sound like some sort of scam hmm uh
+  				// Good question
+          // Just put like "Congrats from: @(usernames...)" where the usernames are just a bunch of bignames on replit. XP
+          // Or something at the end like "For more info: https://repls.best" and on the site some stuff that makes it legit, like vouches or something
+          message = 
+            `Hey, @${data.repl.owner.username}! Congrats on getting your Repl on Trending! You're now eligible for a free \`repls.best\` domain. The default one for your repl is \`${domain}\`. If you would like to claim this, please add \`${domain}\` as a domain in your repl and click verify.\n`;
+        } else {
+          message = `Hey, @${data.repl.owner.username}! Congrats on getting your Repl on Trending! You're now eligible for a free \`repls.best\` domain. The default one for your repl is \`${domain}\`. Since your repl is not a webserver, it will redirect to the spotlight page, and is setup already.\n`;
+        }//   Cosmic was here   
+        console.log(message);
+        
+        // sendMessage(message);
+        
+      }) // Raadsel was also here:)
+    })// CatR3kd was here!
+  })
+}
+
+updateData();
+setInterval(updateData, 60000);
+
+
+
+// DB by CatR3kd
+
+
+
+function saveToDB(repl){
+  // Check for lost data
+  checkData();
+
+  // Get DB and create new entry
+  const db = JSON.parse(fs.readFileSync('Data/DB.json'));
+  const current = JSON.parse(fs.readFileSync('Data/Current.json'));
+  const replName = repl.slug;
+  
+  // Delete old instance of repl
+  if(db.hasOwnProperty(replName)){
+    delete db[replName];
+  }
+  
+  const desc = `${replName.replaceAll('-', ' ')}: A(n) ${repl.language} repl by @${repl.owner.username}! URL: ${repl.hostedUrl} Most recent date: ${getFormattedDate()}`;
+
+  // Add to DB
+  db[replName] = desc;
+  fs.writeFileSync('Data/DB.json', JSON.stringify(db));
+
+  // Add to current
+  current[replName] = desc;
+  fs.writeFileSync('Data/Current.json', JSON.stringify(current));
+  
+  // Update backup file
+  fs.writeFileSync('Data/Backup.json', JSON.stringify(db)); // Backup by stringifying again, just so it's practitcally impossible to have both corrupted.
+
+  // Double check for lost data
+  checkData();
+}
+
+
+function checkData(){
+  if((isEmpty('Data/Backup.json') == true) && (isEmpty('Data/DB.json') == true)){
+    // Only Backup is empty
+    fs.writeFileSync('Data/Backup.json', fs.readFileSync('Data/DB.json'));
+    console.log("Backup data is lost. Syncing with main");
+    return;
+  }
+  
+  if(isEmpty('Data/DB.json') == true){
+    if(isEmpty('Data/Backup.json') == false){
+      // Only DB is empty
+      fs.writeFileSync('Data/DB.json', fs.readFileSync('Data/Backup.json'));
+      console.log("Main data is lost. Syncing with backup.");
+      
+      return;
+    } else if(isEmpty('Data/Backup.json') == true){
+      // Both DB and backup are empty
+      console.log('All data is lost.');
+      fs.writeFileSync('Data/Backup.json', JSON.stringify({}));
+      fs.writeFileSync('Data/DB.json', JSON.stringify({}));
+
+      return;
+    }
+  }
+}
+
+// Check data on start
+checkData();
+
+
+// Check if JSON is empty
+function isEmpty(path) {
+  const file = fs.readFileSync(path);
+  for(var key in file) {
+    if(file.hasOwnProperty(key) && Object.keys(JSON.parse(file)).length !== 0)
+      return false;
+    }
+  return true;
+}
+
+
+// Get current date
+function getFormattedDate() {
+  var date = new Date();
+  return(date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +  date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
+}
+
+
+
+// Server + API by CatR3kd
+
+
+
+const path = require('path');
+const express = require('express');
+const app = express();
+
+
+app.listen(8000, () => {
+  console.log(`App listening on port 8000`);
+});
+
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname + '/Public/index.html'));
+});
+
+app.get('/all', (req, res) => {
+  res.sendFile(path.join(__dirname + '/Data/DB.json'));
+});
+
+app.get('/current', (req, res) => {
+  res.sendFile(path.join(__dirname + '/Data/Current.json'));
+});
---
+++ plan.md
@@ -0,0 +1,27 @@
+# Plan
+Our epic game plan
+---------
+
+| User | What they're doing |
+|:-|:-|
+| haroon | Backend + GraphQL |
+| CosmicBear | Moral Support | <!--lmao -->
+| Raadsel | Front-end + Some back-end + better moral support + fixing/improving some code <!-- I give better moral support then CosmicBear!!1!--> |
+| bddy | uh idk i was just invited lmao | <!--domain stuff? -->
+| PikachuB2005 | INSERT WHAT YOU ARE DOING HERE <!--obviously graphql -cosmic --> | <!-- haroon is doing graphql, idk if I can help with this ;-; -->
+| conspicuous | Front-end |
+| DillonB07 | Frontend help |
+| VulcanWM | Back-end + Some Front-end |
+| codingMASTER398 | Idea man \| Backend + frontend cleanings \| Saying goodmorning at school |
+| Highwayman | INSERT WHAT YOU ARE DOING HERE |
+| CatR3kd | ~~DB~~ creating api | <!-- just more cooler B) --> 
+| sojs | INSERT WHAT YOU ARE DOING HERE |
+
+# What we need done
+- Domain (bddy) <!-- Ayy bddy -->
+- malicious subdomain solution (unassigned) <!-- manual approval? I would use a discord bot + server for that, but nonmanual is preferable -->  <!-- If it's a trending Repl, why not do it automatically? -->
+- replit bot (coding398/haroon)
+- front-end (conspicuous) [Figma prototype](https://www.figma.com/file/MZWlrNJagX0AwV6VkccTO1/repls.best) <!--Maybe that it shows random repls with  a subdomain on the hompage-->
+- DNS STUFF AAAAAAAAAAA (haroon) <!-- should we use cloudflare? i have more experience with that - If you have more experience with that thats great!--> 
+- public api maybe <!--For what then? See when someone got a domain?-->
+- admin panel
